de44d978a8b6fc0eb227cfd47459ff32d7e5aea8
bstanlley <bruno.stanlleyj@gmail.com>
2022-07-09 19:14:07 -0300

    Ajuste do botao clear e correcao de bugs

--- calculadora.js
+++ calculadora.js
@@ -1,52 +1,66 @@
-var botoes = document.getElementsByClassName("botao");
+var botoes = document.querySelectorAll(".botao")
 var limpar = true;
+var stringDisplay = document.querySelector(".resultado").innerText;
 
 
 function escutarBotoes(id){
     document.getElementById(id).addEventListener("click", function(){
-        valorPressionado = document.getElementById(id).innerHTML;
+        valorPressionado = document.querySelector(`#${id}`).innerText;
         display = document.querySelector(".resultado");
-        ultimaPosDisplay = display.innerHTML.length - 1;
+        ultimaPosicString = stringDisplay.length - 1;
         
+        //Botão pressionado foi um número
         if(parseInt(valorPressionado) || valorPressionado == '0'){
             if(limpar == true){
-                display.innerHTML = valorPressionado;
+                stringDisplay = valorPressionado;
             }
             else{
-                display.innerHTML += valorPressionado;
+                stringDisplay += valorPressionado;
             }
 
             limpar = false;
         }
+        //Botão pressionado foi um operador ou botão clear
         else{
-            valorUltPos = display.innerHTML[ultimaPosDisplay];
+            valorUltPos = stringDisplay[ultimaPosicString];
 
             if(valorPressionado == '='){
                 if(parseInt(valorUltPos) || valorUltPos == '0'){
-                    result = eval(display.innerHTML);
+                    result = eval(stringDisplay);
                     
                     if(result % 1 == 0){
-                        display.innerHTML = result;
+                        stringDisplay = result;
                     }
                     else{
-                       display.innerHTML = result.toFixed(2);
+                        stringDisplay = result.toFixed(2);
                     }
                     
                     limpar = true;
                 }
                 else{
-                    display.innerHTML = "Expressão Incorreta";
+                    stringDisplay = "Expressão Incorreta";
                     limpar = true;
                 }
             }
+            else if(valorPressionado == 'C'){
+                stringDisplay = '0';
+                limpar = true;
+            }
             else if(parseInt(valorUltPos) || valorUltPos == '0'){
-                display.innerHTML += valorPressionado;
+                stringDisplay += valorPressionado;
                 limpar = false;
             }
             else{
-                display.innerHTML =  display.innerHTML.substring(0, ultimaPosDisplay) + valorPressionado;
+                stringDisplay =  stringDisplay.substring(0, ultimaPosicString) + valorPressionado;
                 limpar = false;
             }
+        }
+
+        if(stringDisplay.length > 20){
+            display.innerText = stringDisplay.substring(stringDisplay.length-20);
+        }
+        else{
+            display.innerText = stringDisplay;
         }
     })
 }
